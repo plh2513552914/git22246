1d3149fec4683b8da12bc68f73610f56c5f6f86f
plh <2513552914@qq.com>
2021-10-23 19:08:02 +0800

    3333

--- src/main/java/com/plh/Test.java
+++ src/main/java/com/plh/Test.java
@@ -5,6 +5,8 @@ public class Test {
         System.out.println("hello git 111");
         System.out.println("hello git 222");
         System.out.println("hello git 333");
+        System.out.println("master test");
         System.out.println("hot-fix test ");
+        System.out.println("push test ");
     }
 }
